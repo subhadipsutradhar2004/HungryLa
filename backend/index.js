b1764ab79535dd6e5b7c8385111a183ed01d6e02
Subhadip Sutradhar <subhadipsutradhar34@gmail.com>
2026-07-13 17:43:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
    cors:{
-    origin:"http://localhost:5173",
+    origin:"https://hungryla-2j9g.onrender.com",
     credentials:true,
     methods:['POST','GET']
 }
@@ -35,7 +35,7 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://hungryla-2j9g.onrender.com",
     credentials:true
 }))
 app.use(express.json())
