4208336cb4c13d95182d705facd29ccc09c2fcc5
Subhadip Sutradhar <subhadipsutradhar34@gmail.com>
2026-07-13 22:41:41 +0530

    Update firebase.js

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-console.log("Firebase Config:", firebaseConfig);
-console.log("Firebase App:", app.options);
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
